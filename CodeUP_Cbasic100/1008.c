e823b05789f0359c533ca382d926c2825132ff7b
Dalamjwi <80510520+choyunseol@users.noreply.github.com>
2023-05-23 19:56:46 +0900

    Create 1008.c

---
+++ CodeUP_Cbasic100/1008.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("\u250C\u252C\u2510\n");
+    printf("\u251C\u253C\u2524\n");
+    printf("\u2514\u2534\u2518");
+    return 0;
+}
+
+-----
+
+https://codeup.kr/problem.php?id=1008
+\u+유니코드번호 : 유니코드 특수문자 출력
