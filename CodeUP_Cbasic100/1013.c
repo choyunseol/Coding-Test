9af6c719b9e04dff2c24b71cabd5441a88dccd31
Dalamjwi <80510520+choyunseol@users.noreply.github.com>
2023-05-23 20:15:47 +0900

    Create 1013.c

---
+++ CodeUP_Cbasic100/1013.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    scanf("%d%d", &a, &b);
+    printf("%d%d", a,b);
+    return 0;
+}
+
+-----
+
+https://codeup.kr/problem.php?id=1013
+- 두개를 한번에 입력받을 땐 scanf("%d%d", &a, &b); (엔터/공백으로 입력 데이터가 구분되어 입력)
