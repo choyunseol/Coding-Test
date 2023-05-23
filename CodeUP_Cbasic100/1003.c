2177abec95409dca3f6cc5a5bd9f6a0c8997c151
Dalamjwi <80510520+choyunseol@users.noreply.github.com>
2023-05-23 19:31:40 +0900

    Create 1003.c

---
+++ CodeUP_Cbasic100/1003.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello\nWorld");
+    return 0;
+}
+
+-----
+  
+https://codeup.kr/problem.php?id=1003
+
+이스케이프 문자
+- 출력하는 문장 안에서 원하는 형식에 맞추어 출력할 수 있도록 줄을 바꾸는 등의 특별한 의미들을 나타내기 위해 사용한다.
+-'\n'(new line을 의미) : 줄 바꿈
+-'\t' : 탭(tab),
+-'\c' : 캐리지리턴(carriage return, 그 줄의 맨 앞으로 커서를 보냄)
