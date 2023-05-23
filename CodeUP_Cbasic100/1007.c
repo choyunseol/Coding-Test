34f3a14d73197de4fa289d0fe1cb9c6a62c92892
Dalamjwi <80510520+choyunseol@users.noreply.github.com>
2023-05-23 19:54:34 +0900

    Create 1007.c

---
+++ CodeUP_Cbasic100/1007.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("\"C:\\Download\\hello.cpp\"");
+    return 0;
+}
+
+-----
+  
+https://codeup.kr/problem.php?id=1007
+
+printf( ) 함수로 백슬래시 문자 \ 를 출력하기 위해서는 \\ 로 출력해야 한다.
