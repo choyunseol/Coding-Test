8bf2774fd5d237a442f435c75ba3d755d684b189
Dalamjwi <80510520+choyunseol@users.noreply.github.com>
2023-05-23 19:40:15 +0900

    Create 1006.c

---
+++ CodeUP_Cbasic100/1006.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    printf("\"!@#$%%^&*()\"");
+    return 0;
+}
+
+-----
+  
+https://codeup.kr/problem.php?id=1006
+
+% 기호가 앞에 붙는 문자 : 형식 지정자(format specifier)
+- 그 위치에서 지정한 형식으로 값을 출력
+- 퍼센트 문자 %를 출력하기 위해서는 %%로 출력
