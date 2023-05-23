5a55e15662e98131b81dcf24b9acf24be1864957
Dalamjwi <80510520+choyunseol@users.noreply.github.com>
2023-05-23 19:34:52 +0900

    Create 1004.c

---
+++ CodeUP_Cbasic100/1004.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("\'Hello\'");
+    return 0;
+}
+-----
+  
+https://codeup.kr/problem.php?id=1004
+
+printf( ) 함수로 따옴표 문자 ' 를 출력하기 위해서는 \' 로 입력해야 한다.
+\' 는 작은따옴표 문자 자체 출력
+\" 는 큰따옴표 문자 자체 출력
