c6ffe5323688b707068edf64617df391c93745f6
Dalamjwi <80510520+choyunseol@users.noreply.github.com>
2023-05-30 15:01:33 +0900

    Create 1021.c

---
+++ CodeUP_Cbasic100/1021.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char word[51];
+    scanf("%s",&word);
+    printf("%s", word);
+}
+
+-----
+
+https://codeup.kr/problem.php?id=1021
+
+문자열
+- 50개 문자로 이루어진 문자열의 선언 : char data[51]
+- 형식지정자는 %s
