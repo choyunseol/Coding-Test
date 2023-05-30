5cef6fea407d8b5711794964896f9dac34d08d36
Dalamjwi <80510520+choyunseol@users.noreply.github.com>
2023-05-30 14:26:20 +0900

    Create 1018.c

---
+++ CodeUP_Cbasic100/1018.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int hour, min;
+    scanf("%d:%d", &hour, &min);
+    printf("%d:%d", hour, min);
+}
+
+-----
+ 
+https://codeup.kr/problem.php?id=1018
+scanf("%d:%d", &h, &m); //콜론(:) 기호를 기준으로 두 수가 각 변수에 저장된다.
