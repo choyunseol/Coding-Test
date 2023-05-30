bff079313e1a83c73d806205112997fb495f51c7
Dalamjwi <80510520+choyunseol@users.noreply.github.com>
2023-05-30 14:54:36 +0900

    Create 1020.c

---
+++ CodeUP_Cbasic100/1020.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int front, end;
+    scanf("%d-%d", &front, &end);
+    printf("%06d%07d", front, end);
+}
+
+-----
+  
+https://codeup.kr/problem.php?id=1020
